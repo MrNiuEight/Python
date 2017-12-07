233b0c0d66b9fe95801dc7f3f29d8400db6cad76
ZL <708951773@qq.com>
2017-12-07 22:40:12 +0800

    python_study_2017120702

--- Demo1.py
+++ Demo1.py
@@ -1,5 +1,17 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+a = 100
+if a < 0:
+    print(a)
+else:
+    print(-a)
 
-name = input("please input your name")
-print('Hello,', name)
+print('''line1
+line2
+line3''')
+
+print(r'''hello,\n
+world''')
+
+print('%02d-%02d' % (3, 1))
+print('%.5f' % 3.1415926)
