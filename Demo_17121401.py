f70e070f5755bbcbf067dd2baa952ac076787180
ZL <708951773@qq.com>
2017-12-15 20:14:32 +0800

    代码优化

--- Demo_17121401.py
+++ Demo_17121401.py
@@ -21,6 +21,25 @@ def createRed():
         return r3
 
 
+# 生成篮球
+def createBlue():
+    # 有三个区间，每个红球落入区间的概率为1/3；
+    # 骰点
+    dice = random.randint(1, 16)
+    if dice in range(1, 6):
+        # 蓝球第一区间（1~5）
+        b1 = random.randint(1, 5)
+        return b1
+    elif dice in range(6, 12):
+        # 蓝球第二区间（6~11），蓝球无法均分，所以，中间多一个
+        b2 = random.randint(6, 11)
+        return b2
+    else:
+        # 蓝球第三区间（12~16）
+        b3 = random.randint(12, 16)
+        return b3
+
+
 # 产生红球组，调用生成红球的方法6次
 def createReds():
     arr = []
@@ -51,33 +70,49 @@ def createSSQ():
     while True:
         wn += 1
         (reds, t1, t2, t3) = createReds()
+        blue = createBlue()
         # 连号不能大于等于4
         if (reds[0] + 1 != reds[1] and reds[0] + 2 != reds[2] and reds[0] + 3 != reds[3]) and (
                                 reds[1] + 1 != reds[2] and reds[1] + 2 != reds[3] and reds[1] + 3 != reds[4]) and (
                                 reds[2] + 1 != reds[4] and reds[2] + 2 != reds[4] and reds[2] + 3 != reds[5]):
-            ssq = [reds, [random.randint(1, 16)]]
-            print(ssq)
+            ssq = [reds, blue]
             # print("执行次数：" + str(wn))
             # print("t1=" + str(t1) + "；t2=" + str(t2) + "；t3=" + str(t3))
-            break
+            return ssq, t1, t2, t3
 
 
-# 调用函数
-# 生成5组
-num = 0
-sumX = 0
-while True:
-    num += 1
-    now = int(time.time())  # 这是时间戳
-    timeArray = time.localtime(now)
-    otherStyleTime = time.strftime("%H%M%S", timeArray)
-    # 取当前时间的时分秒，产生两个随机数，做一个事件
-    ran1 = random.randint(1, int(otherStyleTime))
-    ran2 = random.randint(1, int(otherStyleTime))
-    if ran1 == ran2:
-        sumX += 1
-        createSSQ()
-        # print(int(otherStyleTime))
-        if sumX == 5:
-            print(num)
-            break
+def mainMethod():
+    # 调用函数
+    # 生成5组
+    num = 0
+    sumX = 0
+    sumNum = 0
+    while True:
+        num += 1
+        sumNum += 1
+        now = int(time.time())  # 这是时间戳
+        timeArray = time.localtime(now)
+        otherStyleTime = time.strftime("%H%M%S", timeArray)
+        # 取当前时间的时分秒，产生两个随机数，做一个事件
+        ran1 = random.randint(1, int(otherStyleTime))
+        ran2 = random.randint(1, int(otherStyleTime))
+        if ran1 == ran2:
+            sumX += 1
+            (ssq, t1, t2, t3) = createSSQ()
+            print(ssq)
+            print("区间发布：" + str(t1) + ":" + str(t2) + ":" + str(t3))
+            print("触发时间：" + otherStyleTime)
+            print("执行次数：" + str(num))
+            num = 0  # 重新计数
+            # print(int(otherStyleTime))
+            if sumX == 5:
+                print("总执行次数：" + str(sumNum))
+                break
+
+
+"""
+    需要补充的
+1、奇偶
+2、AC
+"""
+mainMethod()
